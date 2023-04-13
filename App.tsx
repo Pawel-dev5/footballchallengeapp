2dbc398d10f05bdd065f277c6a31db60756a1eea
PawelNowecki-10a <pawel.nowecki@10a.io>
2023-04-13 17:14:02 +0200

    [F] - Initial configutarion

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+
+const App = () => (
+	<View>
+		<Text>Open up App.js to start working on your app!</Text>
+		<StatusBar style="auto" />
+	</View>
+);
+
+export default App;
